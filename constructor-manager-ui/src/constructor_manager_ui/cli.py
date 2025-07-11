c0487dd9133e38371093b7700a8c4b91e415df8b
jaimergp <jaimergp@users.noreply.github.com>
2025-07-11 13:37:10 +0200

    Adopt ruff for everything (#263)
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>

--- constructor-manager-ui/src/constructor_manager_ui/cli.py
+++ constructor-manager-ui/src/constructor_manager_ui/cli.py
@@ -7,10 +7,10 @@ from constructor_manager_ui.main import main
 
 def run():
     parser = argparse.ArgumentParser()
-    parser.add_argument("package", type=str)
+    parser.add_argument('package', type=str)
     args = parser.parse_args()
     main(args.package)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
