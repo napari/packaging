ac0d8ca2d7f5660d4ef02e841871990f3526004f
jaimergp <jaimergp@users.noreply.github.com>
2025-09-22 14:23:55 +0200

    Delete `constructor-manager` (#291)

--- constructor-manager-cli/src/constructor_manager_cli/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Constructor manager CLI."""
-
-__version__ = '0.0.1'
--- constructor-manager/src/constructor_manager/tests/test_main.py
+++
@@ -1,7 +0,0 @@
-"""Constructor manager API."""
-
-import constructor_manager
-
-
-def test_constructor_manager():
-    assert constructor_manager
